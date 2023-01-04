4da3e8cde0f152476b4fa46739069e92a5f747ce
Anthony Smith <anthony@smith.cm>
2023-01-04 14:51:05 -0800

    Add editorconfig for consistent formatting

--- app/models/calendar.server.ts
+++ app/models/calendar.server.ts
@@ -4,16 +4,16 @@ import type { Calendar } from "@prisma/client";
 export type { Calendar } from "@prisma/client";
 
 export function getCalendar(id: string) {
-  return prisma.calendar.findUnique({ where: { id } });
+    return prisma.calendar.findUnique({ where: { id } });
 }
 
 export function createCalendar(
-  new_calendar: Pick<Calendar, "events" | "remoteUrl">
+    new_calendar: Pick<Calendar, "events" | "remoteUrl">
 ) {
-  return prisma.calendar.create({
-    data: {
-      events: new_calendar.events,
-      remoteUrl: new_calendar.remoteUrl,
-    },
-  });
+    return prisma.calendar.create({
+        data: {
+            events: new_calendar.events,
+            remoteUrl: new_calendar.remoteUrl,
+        },
+    });
 }
--- app/root.tsx
+++ app/root.tsx
@@ -1,38 +1,38 @@
 import type { MetaFunction } from "@remix-run/node";
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+    Links,
+    LiveReload,
+    Meta,
+    Outlet,
+    Scripts,
+    ScrollRestoration,
 } from "@remix-run/react";
 
 import styles from "~/styles/tailwind.css";
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
+    charset: "utf-8",
+    title: "New Remix App",
+    viewport: "width=device-width,initial-scale=1",
 });
 
 export function links() {
-  return [{ rel: "stylesheet", href: styles }];
+    return [{ rel: "stylesheet", href: styles }];
 }
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body className="bg-white sm:bg-slate-100">
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <Meta />
+                <Links />
+            </head>
+            <body className="bg-white sm:bg-slate-100">
+                <Outlet />
+                <ScrollRestoration />
+                <Scripts />
+                <LiveReload />
+            </body>
+        </html>
+    );
 }
